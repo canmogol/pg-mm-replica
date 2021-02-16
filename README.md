fc13dbb6db19fddd41af02540b2a20829739173f
Can MOGOL <canmogol@gmail.com>
2021-02-16 22:57:31 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # pg-mm-replica
 PostgreSQL Master - Master logical bi-directional replication.
+
+## How to run
+
+Just to run `docker-compose up` and three PostgreSQL docker containers start running.
+
+
+## How to enable replication
+
+After docker-compose starts all three PostgreSQL servers, follow these steps to enable bi-directional logical replication on event table.
+
+```
+# connect to the pg1-master and run the following SQL
+CREATE SUBSCRIPTION pg1_subscription_all_tables CONNECTION 'dbname=events host=pg2-master user=replicator password=replicator' PUBLICATION pg2_publication_all_tables;
+
+# connect to the pg2-master and run the following SQL
+CREATE SUBSCRIPTION pg2_subscription_all_tables CONNECTION 'dbname=events host=pg1-master user=replicator password=replicator' PUBLICATION pg1_publication_all_tables;
+```
+
+Now you can insert events into both masters and the data will be replicated.
+
+```
+# on pg1-master
+insert into event(event, type) values ('{"a":1}', 'kv');
+select * from event;
+
+# on pg2-master
+insert into event(event, type) values ('{"b":2}', 'kv');
+select * from event;
+```
+
+Also, you can stop one of the masters and continue to insert events on the other master. The stopped master will replicate the new entries when the it is started again.
+
+```
+# stop the second master
+docker-compose stop pg2-master
+
+# insert new events into pg1-master
+# restart the pg2-master
+docker-compose star pg2-master
+
+# you should see the same events on both masters
+```
+
---
+++ logical/pg1-master/initdb/create-replication-role.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-END
+  create database events;
+  create user events with encrypted password 'events';
+  grant all privileges on database events to events;
+  CREATE ROLE replicator REPLICATION LOGIN PASSWORD 'replicator';
+  grant all privileges on database events to replicator;
+END
+
+psql -v ON_ERROR_STOP=1 --username "events" -d "events"  <<-END
+  create schema if not exists public;
+  create table if not exists public.event
+  (
+    cluster varchar default '1.3.6.1.4.1.56465.100.1'::character varying,
+    id serial,
+    created time default now(),
+    event jsonb not null,
+    type varchar not null,
+    PRIMARY KEY (cluster, id)
+  );
+  alter table event owner to events;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO events;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO replicator;
+END
+
+psql -v ON_ERROR_STOP=1 --username "postgres" -d "events" <<-END
+  CREATE PUBLICATION pg1_publication_all_tables FOR ALL TABLES;
+END
+
+psql -v ON_ERROR_STOP=1 --username "postgres" -d "events" <<-END
+  CREATE OR REPLACE FUNCTION public.resolve()
+      RETURNS trigger
+      LANGUAGE plpgsql
+  AS \$function\$
+  DECLARE
+      id1 int;
+  BEGIN
+      select id into id1 from public.event where cluster = new.cluster and id = new.id;
+      if id1 is not null then
+          RETURN null;
+      end if;
+      RETURN new;
+  END;
+  \$function\$;
+
+  CREATE TRIGGER trigger_before_insert_row BEFORE INSERT ON event FOR EACH ROW EXECUTE PROCEDURE resolve();
+  ALTER TABLE event ENABLE ALWAYS TRIGGER trigger_before_insert_row;
+END
